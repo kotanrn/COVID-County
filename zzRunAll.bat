2ea607d9d8ed023aff6a7f28ad380cac189a9d10
n0beard <kotanr@icloud.com>
2020-05-27 11:55:47 -0500

    To help our Windows friends

---
+++ zzRunAll.bat
@@ -0,0 +1,16 @@
+@echo off
+
+C:\python27\python.exe  County.py 15 "Bell, Texas, US"
+C:\python27\python.exe County.py 15 "Coryell, Texas, US"
+C:\python27\python.exe County.py 15 "Sweden"
+C:\python27\python.exe County.py 15 "Bulgaria"
+C:\python27\python.exe County.py 15 "Hungary"
+C:\python27\python.exe County.py 15 "Latvia"
+C:\python27\python.exe County.py 15 "Lithuania"
+C:\python27\python.exe County.py 15 "Poland"
+C:\python27\python.exe County.py 15 "Romania"
+
+C:\python27\python.exe US_time.py 15
+C:\python27\python.exe Germany_time.py 15
+
+C:\python27\python.exe States.py
